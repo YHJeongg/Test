73c87c4204c4c94e33e220403f11f6c831399a73
JYH <wjd6408@gmail.com>
2022-02-26 18:50:23 +0900

    20220226-1

---
+++ Dev/Swift/baekjoon/입출력과 사칙연산/baekjoon-1000/baekjoon-1000/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  baekjoon-1000
+//
+//  Created by Jeong Yun Hyeon on 2022/02/26.
+//
+
+import Foundation
+
+let input = readLine()
+let inputArray = input!.components(separatedBy: " ")
+
+let a = Int(inputArray[0])!
+let b = Int(inputArray[1])!
+
+print(a + b)
---
+++ baekjoon-1000/baekjoon-1000/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  baekjoon-1000
+//
+//  Created by Jeong Yun Hyeon on 2022/02/26.
+//
+
+import Foundation
+
+print("Hello, World!")
+
