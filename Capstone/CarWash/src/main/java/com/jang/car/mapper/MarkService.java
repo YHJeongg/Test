f7143c3b9ec89c42c8c1f3c5c88db4737668e86b
YunHyeon Jeong <97114061+YHJeongg@users.noreply.github.com>
2022-06-21 20:10:39 +0900

    Delete Capstone/CarWash directory

--- Capstone/CarWash/src/main/java/com/jang/car/mapper/MarkService.java
+++
@@ -1,9 +0,0 @@
-package com.jang.car.mapper;
-
-import java.util.Map;
-
-public interface MarkService {
-	public Map<String, String> retrieveMark(Map<String, String> markParam);
-	public void createMark(Map<String, String> markParam);
-
-}
